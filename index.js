341d7990400c28889205e1e12a92de0b0e7eff3c
klbennett <https://github.com/settings/applications>
2018-10-08 14:42:35 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks (block) {
+  if (block > 42) {
+return block - 42;
+}
+else {
+return 42 - block
+}
+}
+
+function distanceFromHqInFeet(block){
+ return distanceFromHqInBlocks(block) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+
+if (destination > start) {
+  return (destination - start) * 264;
+}
+else {
+return (start - destination) * 264;
+}
+}
+
+function calculatesFarePrice(start, destination) {
+
+  let distanceTravelled = distanceTravelledInFeet(start, destination);
+  if (distanceTravelled < 400) {
+    return 0;
+  } else if (distanceTravelled > 2000 && distanceTravelled < 2500) {
+    return 25;
+  } else if (distanceTravelled > 2500) {
+    return 'cannot travel that far';
+  } else {
+    return ((distanceTravelled-400) * .02);
+  }
+
+  }
